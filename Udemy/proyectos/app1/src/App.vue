d29566d65669998c6ff4e564df6d2460cb74a7d6
jeyson <jeysonrgxd@gmail.com>
2020-06-13 01:36:13 -0500

    primer proyecto de curso de udemy

---
+++ Udemy/proyectos/app1/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app" class="container">
+    <cabecera></cabecera>
+    <lista></lista>
+  </div>
+</template>
+
+<script>
+import cabecera from '@/components/Cabecera.vue'
+import lista from '@/components/lista.vue'
+
+export default {
+  name: 'App',
+  components: {
+    cabecera,
+    lista
+  }
+}
+</script>
+
+
+<style>
+/* sino tiene scope esto sera global, ya que si tuviera scope solo sera estos stylos para este template */
+</style>
