fb9c2f6488503ecff5bc4975f4785d3c1187277f
jeyson <jeysonrgxd@gmail.com>
2020-06-14 17:49:57 -0500

    ejercicio vue cli con vuex

--- Udemy/proyectos/app1/src/components/lista.vue
+++ Udemy/proyectos/app1/src/components/lista.vue
@@ -1,24 +1,34 @@
 <template>
    <div>
       <ul class="list-group">
-         <li class="list-group-item d-flex justify-content-between align-items-center">
-            Cras justo odio
-            <span class="badge badge-primary badge-pill">14</span>
-         </li>
-         <li class="list-group-item d-flex justify-content-between align-items-center">
-            Dapibus ac facilisis in
-            <span class="badge badge-primary badge-pill">2</span>
-         </li>
-         <li class="list-group-item d-flex justify-content-between align-items-center">
-            Morbi leo risus
-            <span class="badge badge-primary badge-pill">1</span>
+         <li v-for="(item,index) of arr_orden" :key="item.id" v-on:click="aumentar(index)"
+            class="list-group-item d-flex justify-content-between align-items-center">
+            {{item.nombre}}
+            <span class="badge badge-primary badge-pill"> {{item.cantidad}} </span>
          </li>
       </ul>
+      <button class="btn btn-danger btn-block" v-on:click="reiniciar">Reiniciar</button>
    </div>
 </template>
 
 <script>
+import {mapState, mapMutations} from 'vuex'
+
+// ver este ejercicio en el curso ya que tambien hay errores
 export default {
-   name:"lista"
+   name:"lista",
+   computed:{
+      ...mapState(['frutas']),
+      
+      arr_orden(){
+         return this.ordenar_array()
+      }
+   },
+   methods: {
+      ...mapMutations(["aumentar","reiniciar"]),
+      ordenar_array(){
+         return this.frutas.sort((a,b)=> b.cantidad - a.cantidad)
+      }
+   },
 }
 </script>
--- Udemy/proyectos/app1/src/store/index.js
+++ Udemy/proyectos/app1/src/store/index.js
@@ -5,8 +5,21 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    frutas:[
+      {nombre:"manzana",cantidad:0},
+      {nombre:"pera",cantidad:0},
+      {nombre:"mandarina",cantidad:0}
+    ]
   },
   mutations: {
+    aumentar(state,id){
+      state.frutas[id].cantidad ++
+    },
+    reiniciar(){
+      this.state.frutas.forEach(element => {
+        element.cantidad = 0
+      });
+    }
   },
   actions: {
   },
